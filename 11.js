c1200b35e2113d10878e2fbf2ab91f2510d53176
Jay <jaeyoung6720@naver.com>
2024-07-18 12:29:45 +0900

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const btnSelect = document.querySelector('.btn-select');
   const listItems = document.querySelectorAll('.list-member ');
+  // const ul = document.querySelector('ul');
+
+  //   btnSelect.addEventListener('click', function () {
+  //   if (ul.style.display == 'none') {
+  //     ul.style.display = 'block';
+  //   } else {
+  //     ul.style.display = 'none';
+  //   }
+  // });
 
   btnSelect.addEventListener('click', function () {
     if (btnSelect.classList.contains('on')) {
